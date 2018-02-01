b97abfe35a002b121bd44e93a08c0aff1093dcd6
Unknown <hsc362@nyu.edu>
2018-02-01 12:23:22 -0500

    02/01 Progression
    1. Create temporary sprites.
    2. Attach WASD directions on to player sprite.
    3. Attach camera follow script on camera.

---
+++ Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 1.5f;
+
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+		spriteRenderer = GetComponent <SpriteRenderer> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+			transform.position += Vector3.up * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.position += Vector3.left * speed * Time.deltaTime;
+
+			spriteRenderer.flipX = false;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.position += Vector3.right * speed * Time.deltaTime;
+
+			spriteRenderer.flipX = true;
+		}
+
+
+		
+	}
+}
