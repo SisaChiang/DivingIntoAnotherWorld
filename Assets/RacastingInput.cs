afe753ae5a5c82f34d1c2d5e3fe230a78f41c79c
Unknown <hsc362@nyu.edu>
2018-02-06 12:08:42 -0500

    02/06 Progression
    1. Incorporate class exercise with project.
      -  raycasting changes the color of game object.
      -  raycasting changes the text of the game object.
    2. Add a timer to the temoporary player sprite.
      -  Add a time UI text to indicates the timer.
    3. Add a Game Over scene.
    4. Load the GameOver scene when the time is up.

---
+++ Assets/RacastingInput.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // inputing the UI library
+
+public class RacastingInput : MonoBehaviour {
+
+	SpriteRenderer diveSprite;
+
+	public Text guiText;
+
+
+	// Use this for initialization
+	// USE THIS IF NEEDED, MOUSE OVER OBJECTS
+	void Start () {
+
+		diveSprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit2D rayCastHit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero); // same as new Vector2 (0,0)
+
+		if (rayCastHit.collider != null) {
+		// the ray has hit something with a collider on it
+			if (rayCastHit.collider.tag == "DiveEquipments") { // check if the name of the collider matches what we are looking for
+				diveSprite.color = Color.blue; // we know are the dive sprites that changes the color.
+				guiText.text = rayCastHit.collider.tag + "Dive Equipment has been hit";
+			} 
+			else {
+				diveSprite.color = Color.white;
+				guiText.text = "Something has been hit but it not the diving equipments";
+			}
+
+		}
+		else {
+
+			diveSprite.color = Color.white;
+			guiText.text = "Nothing hit";
+		}
+
+	}
+}
---
+++ Assets/TickingClock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TickingClock : MonoBehaviour {
+
+
+	float timeLeft = 5.0f; 
+	public Text text; 
+
+	// Use this for initialization
+	void start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		text.text = "Time Left:" + timeLeft.ToString("####");
+		if (timeLeft <= 0) {
+			SceneManager.LoadScene ("GameOver");
+		}
+			
+	}
+}
